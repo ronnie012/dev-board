6601b0c66b0d898424b03c4279a0c755fdaec106
ronnie012 <ronnie.247365.1@gmail.com>
2025-03-02 03:29:33 +0600

    Fine Tuned Overall Functionality Features.

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,4 @@
+// document.addEventListener("DOMContentLoaded", function () {
 document.getElementById("rainbow_theme_btn").addEventListener("click", function () {
   let random_color1 = Math.floor(Math.random()*255);
   let random_color2 = Math.floor(Math.random()*255);
@@ -14,7 +15,49 @@ document.getElementById("current-date").innerText=`${dateToday}`;
 document.getElementById("blog-link").addEventListener("click", function(){
   window.location.href="./blog.discover.html"
 });
-// document.getElementById("back-to-desk").addEventListener("click", function(){
-//   window.location.href="/index.html"
-//   return;
+
+/*
+document.getElementById("back-to-desk").addEventListener("click", function(){
+  window.location.href="/index.html"
+  return;
+});
+*/
+
+let cards = document.getElementsByClassName("card");
+for (let card of cards) {
+  card.addEventListener("click", function (event) {
+    if (event.target.tagName === "BUTTON") {
+      alert("Board updated Successfully");
+
+      let btnCard = event.target;
+      btnCard.disabled = true;
+
+      let taskCountElement = document.getElementById("taskcount-intotal");
+      let taskCount = parseInt(taskCountElement.innerText);
+      taskCountElement.innerText = taskCount - 1;
+
+      let completedTaskElement = document.getElementById("finished-taskcount");
+      let completedTaskCount = parseInt(completedTaskElement.innerText);
+      completedTaskElement.innerText = completedTaskCount + 1;
+
+      let historyMessage = document.createElement("p");
+      historyMessage.className = "bg-blue-100 p-3 mx-2 rounded-xl text-xs";
+
+      let taskName = btnCard.closest(".card").querySelector(".task-name").innerText;
+      historyMessage.innerText = `You have completed the task "${taskName}" at ${new Date().toLocaleTimeString()}`;
+
+      document.getElementById("history").appendChild(historyMessage);
+
+      document.getElementById("messages-cleared").addEventListener("click", function() {
+        document.getElementById("history").innerHTML = "";
+      });
+
+      if (parseInt(taskCountElement.innerText) === 0) {
+        setTimeout(function () {
+          alert("Congratulations! You have completed all the current tasks.");
+        }, 100);
+      }
+    }
+  });
+}
 // });
