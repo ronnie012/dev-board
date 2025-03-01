8a3720dc2ca8e2b6fdb7fc47660278589464ed35
ronnie012 <ronnie.247365.1@gmail.com>
2025-03-01 22:16:17 +0600

    Implemented the Blogs Page with Return Functionality to Homepage.

--- javascript/script.js
+++ javascript/script.js
@@ -5,8 +5,16 @@ document.getElementById("rainbow_theme_btn").addEventListener("click", function 
 
   let random_color = "rgb(" + random_color1 + "," + random_color2 + "," + random_color3 + ", 0.6)";
   document.getElementById("body").style.backgroundColor = random_color;
-})
+});
 
 let time = new Date();
 let dateToday = time.toDateString();
 document.getElementById("current-date").innerText=`${dateToday}`;
+
+document.getElementById("blog-link").addEventListener("click", function(){
+  window.location.href="/blog.discover.html"
+});
+// document.getElementById("back-to-desk").addEventListener("click", function(){
+//   window.location.href="/index.html"
+//   return;
+// });
