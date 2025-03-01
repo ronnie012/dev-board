a1a89da4a5196e94c0dc27ec9405ef8285a8afe5
ronnie012 <ronnie.247365.1@gmail.com>
2025-03-01 18:44:30 +0600

    Implemented Current Date Showing.

--- javascript/script.js
+++ javascript/script.js
@@ -6,3 +6,7 @@ document.getElementById("rainbow_theme_btn").addEventListener("click", function 
   let random_color = "rgb(" + random_color1 + "," + random_color2 + "," + random_color3 + ", 0.6)";
   document.getElementById("body").style.backgroundColor = random_color;
 })
+
+let time = new Date();
+let dateToday = time.toDateString();
+document.getElementById("current-date").innerText=`${dateToday}`;
